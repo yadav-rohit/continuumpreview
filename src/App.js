c92dd4ed81c7c84bd025bb1562339f05c76ef8e1
yadav-rohit <49914319+yadav-rohit@users.noreply.github.com>
2022-02-28 11:27:00 +0530

    pp

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     <TransactionState>
     <div className="flex flex-col md:min-h-screen  overflow-hidden  font-medium  bg-[#000000]">
       <p> Hello</p>
-      <BrowserRouter basename="https://yadav-rohit.github.io/Continuum-Preview/">
+      <BrowserRouter basename="https://yadav-rohit.github.io/continuumpreview/">
       <Header/>
       <Routes>  
       <Route exact path="/" element={<Main/>} />
